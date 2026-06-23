4947b7b0596af42a2548ffa61e2d06f7e31fd94f
paragsince-01 <aggarwalparag5@gmail.com>
2026-06-23 10:25:20 +0530

    folder structure changed

--- api/routes/uploadRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { uploadImage } from '../controllers/uploadController.js';
-import { verifyToken } from '../utils/verifyUser.js';
-
-const router = express.Router();
-
-router.post('/image', verifyToken, uploadImage);
-
-export default router;
